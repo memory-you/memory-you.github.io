d80ffa0cccff9f67257a1961aeee7d041de328c4
memory-you <869529706@qq.com>
2019-11-17 22:39:01 +0800

    Site updated: 2019-11-17 22:38:47

--- dist/music.js
+++ dist/music.js
@@ -2,29 +2,34 @@ const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
   autoplay: false,
-  audio: [{
-      name: "你一定要幸福",
-      artist: '简弘亦',
-      url: 'http://www.ytmp3.cn/down/51689.mp3',
-      cover: 'http://img.ytmp3.cn/image/52.jpg',
+  audio: [
+    {
+      name: 'love is blind',
+      artist: '椎名林檎',
+      url: 'http://music.163.com/song/media/outer/url?id=642439.mp3',
+      cover:
+        'http://p1.music.126.net/elir3LFHdIxocQ6wHICptw==/109951163320745445.jpg'
     },
     {
-      name: '一百万个可能(Live)',
-      artist: '摩登兄弟',
-      url: 'http://www.ytmp3.cn/down/52772.mp3',
-      cover: 'http://img.ytmp3.cn/image/53.jpg',
+      name: '钟无艳',
+      artist: '谢安琪',
+      url: 'http://music.163.com/song/media/outer/url?id=308353.mp3',
+      cover:
+        'http://p1.music.126.net/jKdhcbQIjNTVRVVpc6INQA==/109951163610130313.jpg'
     },
     {
-      name: 'The Rose',
-      artist: 'Westlife',
-      url: 'http://www.ytmp3.cn/down/56694.mp3',
-      cover: 'http://img.ytmp3.cn/image/51.jpg',
+      name: '宙船(そらふね)',
+      artist: 'TOKIO',
+      url: 'http://music.163.com/song/media/outer/url?id=838903.mp3',
+      cover:
+        'http://p1.music.126.net/tBDOMEhMHTlWwB9bt73xiw==/2512384069492582.jpg'
     },
     {
-      name: 'In The Eyes',
-      artist: '江映东',
-      url: 'http://www.ytmp3.cn/down/53053.mp3',
-      cover: 'http://img.ytmp3.cn/image/10.jpg',
+      name: '一番綺麗な私を',
+      artist: '中島美嘉',
+      url: 'http://music.163.com/song/media/outer/url?id=624099.mp3',
+      cover:
+        'http://p1.music.126.net/Dv-wk9uRpwiHZL1ZOdbtWw==/109951163825340682.jpg'
     }
   ]
-});
+})
